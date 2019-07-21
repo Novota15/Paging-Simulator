e82a13822c13ffc02d03ee0337b017e6fce18020
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-07-21 16:06:00 -0600

    update

--- pager-lru.c
+++ pager-lru.c
@@ -15,7 +15,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
     int proctmp;
     int pagetmp;
     int pg_counter;
-    int my_pg;
+    int curr_pg;
     int proc_val;
     int new_pg;
     int min_val;
@@ -33,31 +33,32 @@ void pageit(Pentry q[MAXPROCESSES]) {
     }
     // LRU paging implementation
 
-    for(proc_val = 0; proc_val < MAXPROCESSES; proc_val++) { 
+    for(proc_val = 0; proc_val < MAXPROCESSES; proc_val++) {
+        // keep looping if process is not active
         if(!q[proc_val].active) {
             continue;
         }
         else {
             pg_counter = q[proc_val].pc;         
-            my_pg = pg_counter/PAGESIZE;
-            timestamps[proc_val][my_pg] = tick;
-            if(!q[proc_val].pages[my_pg]) {
+            curr_pg = pg_counter/PAGESIZE;
+            timestamps[proc_val][curr_pg] = tick;
+            if(!q[proc_val].pages[curr_pg]) {
                 // check to see if swap is possible and use LRU
-                if(!pagein(proc_val, my_pg)) {
+                if(!pagein(proc_val, curr_pg)) {
                     min_val = tick;
                     for(new_pg = 0; new_pg < MAXPROCPAGES; new_pg++) {
+                        // keep looping if new page not in pages
                         if(!q[proc_val].pages[new_pg]) {
                             continue;
                         }
                         else {
                             if(timestamps[proc_val][new_pg] < min_val) {
-                                // setting lowest timestamp
-                                min_val = timestamps[proc_val][new_pg];
-                                min_pg = new_pg; // setting lowest page
+                                min_val = timestamps[proc_val][new_pg]; // set lowest timestamp
+                                min_pg = new_pg; // set lowest page
                             }
                         }
                     }
-                    // if pageout returns failure then exit
+                    // if pageout returns failure, exit
                     if(!pageout(proc_val, min_pg)) {
                         printf("*** Error! ***\n");
                     }
--- pager-predict.c
+++ pager-predict.c
@@ -4,114 +4,115 @@
 #include "simulator.h"
 
 void pager_predict(int*, int, int);
-void set_to_one_A(int*, int*);
-void set_to_one_B(int*, int*, int*);
+void set_to_one_2(int*, int*);
+void set_to_one_3(int*, int*, int*);
 
-void pager_predict(int* predict, int my_pg, int my_prog_var) {
+void pager_predict(int* predict, int curr_pg, int my_prog_var) {
     if(my_prog_var == 0) {
-        if(my_pg == 2) {
-            set_to_one_B(&predict[3], &predict[4], &predict[10]);
+        if(curr_pg == 2) {
+            set_to_one_3(&predict[3], &predict[4], &predict[10]);
         }
-        else if(my_pg == 3) {
-            set_to_one_B(&predict[4], &predict[10], &predict[11]);
+        else if(curr_pg == 3) {
+            set_to_one_3(&predict[4], &predict[10], &predict[11]);
         }
-        else if(my_pg == 10) {
-            set_to_one_A(&predict[0], &predict[11]);
+        else if(curr_pg == 10) {
+            set_to_one_2(&predict[0], &predict[11]);
         }
-        else if(my_pg == 11) {
-            set_to_one_A(&predict[0], &predict[1]);
+        else if(curr_pg == 11) {
+            set_to_one_2(&predict[0], &predict[1]);
         }
         else {
-            set_to_one_A(&predict[my_pg + 1], &predict[my_pg + 2]);
+            set_to_one_2(&predict[curr_pg + 1], &predict[curr_pg + 2]);
         }
     }
     else if(my_prog_var == 1) {
-        if(my_pg == 7) {
-            set_to_one_A(&predict[0], &predict[8]);
+        if(curr_pg == 7) {
+            set_to_one_2(&predict[0], &predict[8]);
         }
-        else if(my_pg == 8) {
-            set_to_one_A(&predict[0], &predict[1]);
+        else if(curr_pg == 8) {
+            set_to_one_2(&predict[0], &predict[1]);
         }
         else {
-            set_to_one_A(&predict[my_pg + 1], &predict[my_pg + 2]);
+            set_to_one_2(&predict[curr_pg + 1], &predict[curr_pg + 2]);
         }
     }
     else if(my_prog_var == 2) {
-        if(my_pg == 12) {
-            set_to_one_A(&predict[9], &predict[13]);
+        if(curr_pg == 12) {
+            set_to_one_2(&predict[9], &predict[13]);
         }
-        else if(my_pg == 13) {
-            set_to_one_B(&predict[0], &predict[9], &predict[10]);
+        else if(curr_pg == 13) {
+            set_to_one_3(&predict[0], &predict[9], &predict[10]);
         }
         else {
-            set_to_one_A(&predict[my_pg + 1], &predict[my_pg + 2]);
+            set_to_one_2(&predict[curr_pg + 1], &predict[curr_pg + 2]);
         }
     }
     else if(my_prog_var == 3) {
-        if(my_pg == 13) {
-            set_to_one_A(&predict[0], &predict[14]);
+        if(curr_pg == 13) {
+            set_to_one_2(&predict[0], &predict[14]);
         }
-        else if(my_pg == 14) {
-            set_to_one_A(&predict[0], &predict[1]);
+        else if(curr_pg == 14) {
+            set_to_one_2(&predict[0], &predict[1]);
         }
         else {
-            set_to_one_A(&predict[my_pg+1], &predict[my_pg+2]);
+            set_to_one_2(&predict[curr_pg+1], &predict[curr_pg+2]);
         }
     }
     else if(my_prog_var == 4) {
-        if(my_pg == 2) {
-            set_to_one_A(&predict[0], &predict[3]);
+        if(curr_pg == 2) {
+            set_to_one_2(&predict[0], &predict[3]);
         }
-        else if(my_pg == 3) {
-            set_to_one_A(&predict[0], &predict[1]);
+        else if(curr_pg == 3) {
+            set_to_one_2(&predict[0], &predict[1]);
         }
         else {
-            set_to_one_A(&predict[my_pg+1], &predict[my_pg+2]);
+            set_to_one_2(&predict[curr_pg+1], &predict[curr_pg+2]);
         }
     }
     else {
         // if none of the prog_vars -> id the page and run all 
-        if(my_pg == 2) {
-            set_to_one_B(&predict[3], &predict[4], &predict[10]);
+        if(curr_pg == 2) {
+            set_to_one_3(&predict[3], &predict[4], &predict[10]);
         }
-        else if(my_pg == 3) {
-            set_to_one_A(&predict[4], &predict[10]);
+        else if(curr_pg == 3) {
+            set_to_one_2(&predict[4], &predict[10]);
         }
-        else if(my_pg == 7) {
-            set_to_one_B(&predict[0], &predict[8], &predict[9]);
+        else if(curr_pg == 7) {
+            set_to_one_3(&predict[0], &predict[8], &predict[9]);
         }
-        else if(my_pg == 8) {
-            set_to_one_B(&predict[0], &predict[9], &predict[10]);
+        else if(curr_pg == 8) {
+            set_to_one_3(&predict[0], &predict[9], &predict[10]);
         }
-        else if(my_pg == 10) {
-            set_to_one_B(&predict[0], &predict[11], &predict[12]);
+        else if(curr_pg == 10) {
+            set_to_one_3(&predict[0], &predict[11], &predict[12]);
         }
-        else if(my_pg == 11) {
-            set_to_one_B(&predict[0], &predict[12], &predict[13]);
+        else if(curr_pg == 11) {
+            set_to_one_3(&predict[0], &predict[12], &predict[13]);
         }
-        else if(my_pg == 12) {
-            set_to_one_A(&predict[0], &predict[9]);
-            set_to_one_A(&predict[13], &predict[14]);
+        else if(curr_pg == 12) {
+            set_to_one_2(&predict[0], &predict[9]);
+            set_to_one_2(&predict[13], &predict[14]);
         }
-        else if(my_pg == 13) {
-            set_to_one_A(&predict[0], &predict[9]);
-            set_to_one_A(&predict[10], &predict[14]);
+        else if(curr_pg == 13) {
+            set_to_one_2(&predict[0], &predict[9]);
+            set_to_one_2(&predict[10], &predict[14]);
         }
         else {
-            set_to_one_A(&predict[my_pg+1], &predict[my_pg+2]);
+            set_to_one_2(&predict[curr_pg+1], &predict[curr_pg+2]);
         }
     }
 }
 
-void set_to_one_A(int* num_one, int* num_two) {
-    *num_one = 1; // setting num_one to 1
-    *num_two = 1; // setting num_two to 1
+// functions that set value to one
+void set_to_one_2(int* a, int* b) {
+    *a = 1; // setting a to 1
+    *b = 1; // setting b to 1
 }
-
-void set_to_one_B(int* num_one, int* num_two, int* num_three) {
-    *num_one = 1; // setting num_one to 1
-    *num_two = 1; // setting num_two to 1
-    *num_three = 1; // setting num_three to 1
+// has one more input than A
+void set_to_one_3(int* a, int* b, int* c) {
+    *a = 1; // setting a to 1
+    *b = 1; // setting b to 1
+    *c = 1; // setting c to 1
 }
 
 void pageit(Pentry q[MAXPROCESSES]) { 
@@ -126,7 +127,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
     /* Local vars */
     int proctmp;
     int p_counter;
-    int my_pg;
+    int curr_pg;
     int proc_val;
 
     /* initialize static vars on first run */
@@ -146,33 +147,33 @@ void pageit(Pentry q[MAXPROCESSES]) {
             continue; // do nothing
         }
         else { // if the process is active
-            int pg_predict[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // max_pc only requires a pg num of 15
+            int pg_predict[15] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // max_pc only requires a page number of 15
             p_counter = q[proc_val].pc;              
-            my_pg = p_counter/PAGESIZE;         
-            pg_predict[my_pg] = 1;
+            curr_pg = p_counter/PAGESIZE;         
+            pg_predict[curr_pg] = 1;
 
-            // selecting prog based on the prev pg
-            if(my_pg == 0 && prev_pg[proc_val] == 8) {
+            // selecting program based on the prev pg
+            if(curr_pg == 0 && prev_pg[proc_val] == 8) {
                 selection[proc_val] = 1;
             }
-            else if(my_pg == 0 && prev_pg[proc_val] == 3) { 
+            else if(curr_pg == 0 && prev_pg[proc_val] == 3) { 
                 selection[proc_val] = 4;  
             }
-            else if(my_pg == 9 && prev_pg[proc_val] == 13) {
+            else if(curr_pg == 9 && prev_pg[proc_val] == 13) {
                 selection[proc_val] = 2;  
             } 
-            else if((prev_pg[proc_val] == 11 && my_pg == 0) || (prev_pg[proc_val] == 3 && my_pg == 10)) {
+            else if((prev_pg[proc_val] == 11 && curr_pg == 0) || (prev_pg[proc_val] == 3 && curr_pg == 10)) {
                 selection[proc_val] = 0;
             }
-            else if(my_pg == 14) {
+            else if(curr_pg == 14) {
                 selection[proc_val] = 3;
             }
 
-            pager_predict(pg_predict, my_pg, selection[proc_val]); // function that makes a prediction
+            pager_predict(pg_predict, curr_pg, selection[proc_val]); // my prediction function
             // 1911/128 < 15
             for(int i = 0; i < 15; i++) {
                 if(pg_predict[i]) {
-                    // swap the page if 'tis predicted
+                    // swap the page if predicted
                     if(q[proc_val].pages[i]) {
                        continue;
                     }
@@ -182,7 +183,7 @@ void pageit(Pentry q[MAXPROCESSES]) {
                     
                 }
                 else {
-                    // doing a page out if the page is not predicted
+                    // page out if page not predicted
                     if(q[proc_val].pages[i]) {
                         if(pageout(proc_val, i)) {
                             continue;
@@ -195,8 +196,8 @@ void pageit(Pentry q[MAXPROCESSES]) {
                     }
                 }
             }
-            // set the previous pg
-            prev_pg[proc_val] = my_pg;
+            // set the previous page
+            prev_pg[proc_val] = curr_pg;
         }
     }
 
